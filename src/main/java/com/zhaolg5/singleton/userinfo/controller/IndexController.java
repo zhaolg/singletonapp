ea2b73321c9a048d0971766a50d6568492933264
lingang.zhaolg <lingang.zhao@ximalaya.com>
2019-09-29 20:10:06 +0800

    更改配置

--- src/main/java/com/zhaolg5/singleton/userinfo/controller/IndexController.java
+++ src/main/java/com/zhaolg5/singleton/userinfo/controller/IndexController.java
@@ -3,7 +3,6 @@ package com.zhaolg5.singleton.userinfo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author : lingangzhao
